9dd0bdf0568c6a3c0588a2dac500fd79776316c9
Herelius <youcefboutrig92@gmail.com>
2025-02-28 14:10:36 +0100

    Add a view that provide us a way to create a new blog posts and to get all the blog posts that exists.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# generics contains generic views to update delete, etc..., any type of model
+from rest_framework import generics
+
+from .models import BlogPost
+from .serializer import BlogPostSerializer
 
 # Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+    # Get all the BlogPost that exists
+    queryset = BlogPost.objects.all()
+    # Specify the serilizer
+    serializer_class = BlogPostSerializer

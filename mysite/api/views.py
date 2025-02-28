2556d26ad393df31797c06e5c47cf55b6f6513a8
Herelius <youcefboutrig92@gmail.com>
2025-02-28 14:51:08 +0100

    - Update .gitignore. - Add a delete/update/retrieve view to the url "blogposts/<int:pk>/" - Add a delete button to the blogposts view.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 # generics contains generic views to update delete, etc..., any type of model
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import BlogPost
 from .serializer import BlogPostSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
@@ -11,3 +13,12 @@ class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     # Specify the serilizer
     serializer_class = BlogPostSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk" # "pk" = primary key (id of the BlogPost in this case)

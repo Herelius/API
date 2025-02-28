783933452501a5665a946061b40210c318cda068
Herelius <youcefboutrig92@gmail.com>
2025-02-28 14:02:59 +0100

    Create a serializer : Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, allowing parsed data to be converted back into complex types, after first validating the incoming data.

---
+++ mysite/api/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import BlogPost
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: BlogPost
+        # Specify the content of the model that we want the api to return
+        fields: ["id", "title", "content", "published_date"]
